28a010ab479bf487b2347256a93b13249a7904d3
mariomulansky <mario.mulansky@gmx.net>
2013-06-23 12:36:37 -0500

    +1d openmp

---
+++ osc_chain_1d/openmp/system.hpp
@@ -0,0 +1,104 @@
+/* stronlgy nonlinear hamiltonian lattice in 2d */
+
+#ifndef LATTICE2D_HPP
+#define LATTICE2D_HPP
+
+#include <vector>
+#include <cmath>
+#include <iostream>
+
+#include <omp.h>
+
+#include <boost/math/special_functions/sign.hpp>
+
+
+namespace checked_math {
+    inline double pow( double x , double y )
+    {
+        if( x==0.0 )
+            // 0**y = 0, don't care for y = 0 or NaN
+            return 0.0;
+        using std::pow;
+        using std::abs;
+        return pow( abs(x) , y );
+    }
+}
+
+double signed_pow( double x , double k )
+{
+    using boost::math::sign;
+    return checked_math::pow( x , k ) * sign(x);
+}
+
+
+struct osc_chain {
+
+    const double m_beta;
+    const double m_kap;
+    const double m_lam;
+    int m_threads;
+
+    osc_chain( const double kap , const double lam , 
+            const double beta )
+        : m_kap( kap ) , m_lam( lam ) , m_beta( beta ) , 
+          m_threads(0)
+    { }
+
+    template< class StateIn , class StateOut >
+    void operator()( const StateIn &q , StateOut &dpdt )
+    {
+        // std::cout << "system" << std::endl;
+        // q and dpdt are 2d
+        const int N = q.size();
+
+        double coupling_lr( 0.0 );
+        int last_i = -1;
+
+#ifndef NO_OMP
+#pragma omp parallel for firstprivate( coupling_lr , last_i ) schedule(runtime)
+#endif	
+        for( int i=0 ; i<N ; ++i )
+        {
+            // non-continuous execution
+            if( i != (last_i+1) )
+            {
+                coupling_lr = signed_pow( q[i-1]-q[i] , m_lam-1 );
+            }
+            dpdt[i] = -signed_pow( q[i] , m_kap-1 )
+                + coupling_lr;
+            coupling_lr = signed_pow( q[i]-q[i+1] , m_lam-1 );
+            dpdt[i] -= coupling_lr;
+            last_i = i;
+        }
+    }
+
+    template< class StateIn >
+    double energy( const StateIn &q , const StateIn &p )
+    {
+        using checked_math::pow;
+        // q and dpdt are 2d
+        const size_t N = q.size();
+        double energy = 0.0;
+#ifndef NO_OMP
+#pragma omp parallel
+        {
+#pragma omp master
+            {
+                if( m_threads == 0 )
+                    m_threads = omp_get_num_threads();
+            }
+
+#pragma omp for reduction(+:energy) schedule(runtime)
+#endif //NO_OMP
+            for( size_t i=0 ; i<N-1 ; ++i )
+            {
+                energy += p[i]*p[i] / 2.0
+                    + pow( q[i] , m_kap ) / m_kap
+                    + pow( q[i]-q[i+1] , m_lam ) / m_lam;
+            }
+        }
+        return energy;
+    }
+};
+
+#endif
---
+++ osc_chain_1d/openmp/test_omp.cpp
@@ -0,0 +1,91 @@
+// Copyright 2013 Mario Mulansky
+
+#include <iostream>
+#include <vector>
+#include <random>
+
+#include <boost/numeric/odeint.hpp>
+#include <boost/timer/timer.hpp>
+#include <boost/foreach.hpp>
+
+#include "system.hpp"
+#include "omp_algebra.hpp"
+
+using boost::numeric::odeint::symplectic_rkn_sb3a_mclachlan;
+
+using boost::timer::auto_cpu_timer;
+using boost::timer::cpu_times;
+
+typedef std::vector< double > state_type;
+
+typedef symplectic_rkn_sb3a_mclachlan< state_type ,
+                                       state_type ,
+                                       double ,
+                                       state_type ,
+                                       state_type , 
+                                       double ,
+                                       omp_algebra > stepper_type;
+
+const double KAPPA = 3.5;
+const double LAMBDA = 4.5;
+const double beta = 1.0;
+
+int main( int argc , char* argv[] )
+{
+    int N = 1024;
+    int steps = 100;
+    double dt = 0.01;
+    if( argc > 1 )
+        N = atoi( argv[1] );
+    int block_size = N/4;
+    if( argc > 2 )
+        block_size = atoi( argv[2] );
+    if( argc > 3 )
+        steps = atoi( argv[3] );
+    if( argc > 4 )
+        dt = atof( argv[4] );
+
+    std::cout << "Size: " << N << " with " << block_size << " elements per thread" << std::endl;
+
+    //omp_set_schedule( omp_sched_dynamic , block_size );
+    omp_set_schedule( omp_sched_static , block_size );
+
+    // initialize
+    state_type p_init( N , 0.0 );
+
+    // fully random
+    std::uniform_real_distribution<double> distribution( 0.0 );
+    std::mt19937 engine( 0 ); // Mersenne twister MT19937
+    auto generator = std::bind( distribution , engine );
+    std::generate( p_init.begin() , p_init.end() , generator );
+    
+    state_type q( N );
+    state_type p( N );
+    
+#pragma omp parallel for schedule( runtime )
+    for( size_t i=0 ; i<N ; i++ )
+    {
+        q[i] = 0.0;
+        p[i] = p_init[i];
+    }
+
+    osc_chain sys( KAPPA , LAMBDA , beta );
+
+    std::cout << "Initial energy: " << sys.energy( q , p ) << std::endl;
+
+    {
+    auto_cpu_timer timer( 3 , "%w sec\n");
+
+    integrate_n_steps( stepper_type() , 
+                       sys , 
+                       std::make_pair( std::ref(q) , std::ref(p) ) , 
+                       0.0 , dt , steps );
+                       //std::ref(obs) );
+
+    }
+
+    // std::cout << "Time: " << elapsed << std::endl;
+    std::cout << "Final energy: " << sys.energy( q , p ) << std::endl;
+
+    return 0;
+}
